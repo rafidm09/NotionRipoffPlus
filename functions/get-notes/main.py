9f52b3869478e7f014488b1f9bef76a577b1a113
NoshinZion <noshin.zion@ucalgary.ca>
2023-03-29 00:12:20 -0600

    Added the lambda functions

--- functions/get-notes/main.py
+++ functions/get-notes/main.py
@@ -1,1 +1,15 @@
-# add your get-notes function here
+import json
+import boto3
+
+dynamodb_resource = boto3.resource('dynamodb')
+table = dynamodb_resource.Table('lotion-30152524')
+
+response = table.get_item(
+    Key={
+        'email': 'niggamoney@420.com',
+        'id': 'cock_enthusiast'
+    }
+)
+
+item = response['Item']
+print(item)
--- functions/save-note/main.py
+++ functions/save-note/main.py
@@ -1,1 +1,13 @@
-# add your save-note function here
+import json
+import boto3
+
+dynamodb_resource = boto3.resource('dynamodb')
+table = dynamodb_resource.Table('lotion-30152524')
+
+note = {
+        'email': 'niggamoney@420.com',
+        'id': 'cock_enthusiast',
+        'body': 'Out here with lotion on my dick rn cuh, jerking my dick off cuh',
+    }
+
+table.put_item(Item=note)
